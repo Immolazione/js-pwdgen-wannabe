2b372b5703916512cf4ba3fbb96d6e62e56f3e86
Immolazione <federico.deered@gmail.com>
2021-12-20 17:32:19 +0100

    add variables

--- js/script.js
+++ js/script.js
@@ -5,8 +5,16 @@ console.log("JS OK");
 // COLORE PREFERITO
 // STAMPA
 
-passGenerated = `${myName} ${mySurname} ${myColor}21`;
+const myName = prompt('Qual è il tuo nome?');
+console.log(myName);
+
+const mySurname = prompt('Qual è il tuo cognome?');
+console.log(mySurname);
+
+const myColor = prompt('Qual è il tuo colore preferito?');
+console.log(myColor);
+
+
+passGenerated = `${myName}${mySurname}${myColor}21`;
 
 console.log(passGenerated);
-
-document.getElementById(passGenerated).innerHTML;
